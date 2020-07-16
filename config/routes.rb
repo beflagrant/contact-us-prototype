89434e6ce3f01f76ea03c2db0f263cddf94bfaed
Ben Vandgrift <ben@vandgrift.com>
2020-07-16 18:24:42 -0400

    provides minimal styling

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
 
   resources :intakes, only: [:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: "pages#index"
 end
