a4de6527878afd988cda4589615e023887503f68
Jonathan Greenberg <jonathan.s.greenberg@gmail.com>
2020-07-24 14:06:55 -0400

    Adds APPLICATION_HOST configuration for production urls
    [#2864067212] Deploy To Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,8 +32,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = 'http://assets.example.com'
-
+  config.action_controller.asset_host = ENV.fetch("APPLICATION_HOST")
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
@@ -75,6 +74,8 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
+  config.action_controller.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
